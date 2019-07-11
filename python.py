b1793891be4da18cc5689922f1920d4336ecf631
AMIT PANDEY <amitpaypal123121@gmail.com>
2019-07-11 16:05:04 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+name = input(" What is your name?: \n")
+print("My name is :", name)
+
+
+
+a = float(input("enter first number: \n "))
+b = int(input("enter second number: \n"))
+ 
+sum = a + b
+ 
+print("sum:", sum)
+
+
